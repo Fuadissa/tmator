332e6fcd9d2b332d46f7b7000e428d0116b60a0a
Fuad <issafuad1818@gmail.com>
2024-11-06 17:30:59 -0800

    beta testing

--- src/app/api/user/[tgId]/route.tsx
+++ src/app/api/user/[tgId]/route.tsx
@@ -2,23 +2,23 @@ import connect from "@/lib/db";
 import User from "@/lib/modal/userSchema";
 import { NextResponse } from "next/server";
 
-export async function GET(
-  request: Request,
-  context: { params: { tgId: string } }
-) {
-  const { tgId } = context.params;
-
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const tgId = searchParams.get("tgId");
   try {
     await connect();
 
-    const tgIdUser = await User.findById({ tg_id: tgId });
-
-    if (!tgIdUser) {
-      return new NextResponse("User Tg Id not found", { status: 404 });
+    // Check if appDataId is provided in the route
+    if (tgId) {
+      const tgIdUser = await User.findById({ tg_id: tgId });
+      if (!tgId) {
+        return new NextResponse("User Tg Id not found", { status: 404 });
+      }
+      return new NextResponse(JSON.stringify(tgIdUser), { status: 200 });
+    } else {
+      return new NextResponse("User Tg Id is required", { status: 400 });
     }
-
-    return new NextResponse(JSON.stringify(tgIdUser), { status: 200 });
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     return new NextResponse("Error fetching User: " + error.message, {
       status: 500,
