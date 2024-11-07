9d89d5178cafcd415872dd9c022b6bbc801f333f
Fuad <issafuad1818@gmail.com>
2024-11-06 17:26:06 -0800

    beta testing

--- src/app/api/user/[tgId]/route.tsx
+++ src/app/api/user/[tgId]/route.tsx
@@ -7,20 +7,18 @@ export async function GET(
   context: { params: { tgId: string } }
 ) {
   const { tgId } = context.params;
+
   try {
     await connect();
 
-    // Check if appDataId is provided in the route
-    if (tgId) {
-      const tgIdUser = await User.findById({ tg_id: tgId });
-      if (!tgId) {
-        return new NextResponse("User Tg Id not found", { status: 404 });
-      }
-      return new NextResponse(JSON.stringify(tgIdUser), { status: 200 });
-    } else {
-      return new NextResponse("User Tg Id is required", { status: 400 });
+    const tgIdUser = await User.findById({ tg_id: tgId });
+
+    if (!tgIdUser) {
+      return new NextResponse("User Tg Id not found", { status: 404 });
     }
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+
+    return new NextResponse(JSON.stringify(tgIdUser), { status: 200 });
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     return new NextResponse("Error fetching User: " + error.message, {
       status: 500,
